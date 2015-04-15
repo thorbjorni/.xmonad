c7a963154fda998504ee0565e461674f202984f9
Thorbjorn Smari Ivarsson <smari@basis.is>
2015-04-15 14:34:17 +0000

    My xmonad.hs config

--- xmonad.hs
+++ xmonad.hs
@@ -7,7 +7,6 @@ import Data.Ratio
 import GHC.Real
 import System.Exit
 import XMonad
---import XMonad
 import XMonad.StackSet hiding (workspaces)
 import XMonad.Actions.SpawnOn
 import XMonad.Actions.Warp
